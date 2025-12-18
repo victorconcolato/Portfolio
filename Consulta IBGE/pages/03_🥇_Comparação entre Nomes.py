5d4cda5a64f3bafe2729a423eeae5d72cfc4a7d6
victorconcolato <victorconcolato@gmail.com>
2025-12-18 16:46:39 -0300

    Update 03_🥇_Comparação entre Nomes.py
    Ajuste final de cacheamento, refatoração e correção de erros na pagina de comparação entre nomes

--- Consulta IBGE/pages/03_🥇_Comparação entre Nomes.py
+++ Consulta IBGE/pages/03_🥇_Comparação entre Nomes.py
@@ -5,77 +5,69 @@ import pandas as pd
 
 st.markdown('# Comparação entre nomes')
 st.divider()
+#Criação das colunas 
 col00, col10 = st.columns(2)
+#Gravação dos dados na sessão com tratamento de acentos e colocando todos em letra maiuscula
+st.session_state['nome_0'] = col00.text_input('Digite o primeiro nome',key=0).upper().replace("Ã","A").replace("Á","A").replace("É","E").replace("Í","I").replace("Ó","O").replace("Ú","U").replace("Ç","C").upper()
+st.session_state['nome_1'] = col10.text_input('Digite o segundo nome',key=1).upper().replace("Ã","A").replace("Á","A").replace("É","E").replace("Í","I").replace("Ó","O").replace("Ú","U").replace("Ç","C").upper()
 
-st.session_state['nome_0'] = col00.text_input('Digite o primeiro nome',key=0).upper().replace("Ã","A").replace("Á","A").replace("É","E").replace("Í","I").replace("Ó","O").replace("Ú","U").replace("Ç","C")
-st.session_state['nome_1'] = col10.text_input('Digite o segundo nome',key=1).upper().replace("Ã","A").replace("Á","A").replace("É","E").replace("Í","I").replace("Ó","O").replace("Ú","U").replace("Ç","C")
-
+#Criação de variável para as chaves gravadas na sesão
 nome_0 = st.session_state['nome_0']
 nome_1 = st.session_state['nome_1']
 enviar = st.button('Enviar')
+
+
 st.divider()
 try:
-    @st.cache_data
-    def requisicao_ibge():
-        url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome_0}|{nome_1}"
-        resposta = requests.get(url)
-        return resposta.json()
-    
-    resposta = requisicao_ibge()
-
+    #Caso aperte o botao enviar e os dois nomes estejam vazios, FAÇA:
     if enviar and nome_0!="" and nome_1!="":
+    #Cacheamento de dados para solicitar os dados somente uma vez
+        @st.cache_data
+        def requisicao_ibge(nome_0,nome_1):
+            url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome_0}|{nome_1}"
+            resposta = requests.get(url)
+            return resposta.json()
+        #Gravação da execução dos dados na variavel resposta
+        resposta = requisicao_ibge(nome_0,nome_1)
+        
+        dicionario_referencia = {resposta[0]['nome']:resposta[0]['res'],
+                                 resposta[1]['nome']:resposta[1]['res']
+                                 }
         
 #Se resposta não for vazio, FAÇA:
         if resposta:
+            dicionario_full = pd.DataFrame({
+                'Período':[x['periodo'] for x in dicionario_referencia[nome_0]],
+                
+                f'Frequência | {nome_0.capitalize()}':[x['frequencia'] for x in dicionario_referencia[nome_0]],
+                
+                f'Frequência | {nome_1.capitalize()}':[x['frequencia'] for x in dicionario_referencia[nome_1]]
+                })
             
-#Se o primeiro valor da lista tiver com o nome igual ao enviado pelo usuário, FAÇA:
-#Este if/else, evita o problema da API enviar os dados trocados.
-#Caso o valor coincida com o JSON enviado pela API, faça:
-
-            if nome_0 == resposta[0]['nome']:
-                tot_freq_nome_0 = sum(item['frequencia'] for item in resposta[0]['res']) 
-                tot_freq_nome_1 = sum(item['frequencia'] for item in resposta[1]['res'])
-
-#Caso a API envie os dados contrários(nome_01 como segundo item da lista), FAÇA:
-#Esta forma é possível pois só há 2 valores.
-            else:
-                tot_freq_nome_0 = sum(item['frequencia'] for item in resposta[1]['res'])
-                tot_freq_nome_1 = sum(item['frequencia'] for item in resposta[0]['res'])
+            dicionario_full['Nome mais popular'] = dicionario_full.apply(lambda row: nome_0.capitalize() if row[f'Frequência | {nome_0.capitalize()}'] > row[f'Frequência | {nome_1.capitalize()}'] else nome_1.capitalize(), axis=1 )
+            dicionario_full['Período'] = dicionario_full['Período'].str.replace("[","")
+            dicionario_full.set_index('Período',inplace=True)
             
-            
+#Widget de FREQUÊNCIA TOTAL
             col01, col11 = st.columns(2)
             
             col01.markdown(f'#### Frequência Total | {nome_0.capitalize()}')
-            col01.markdown(f'### {tot_freq_nome_0:,}'.replace(",", "."))
+            
+            col01.markdown(f'### {dicionario_full[f'Frequência | {nome_0.capitalize()}'].sum():,}'.replace(",", "."))
 
             col11.markdown(f'#### Frequência Total | {nome_1.capitalize()}')
-            col11.markdown(f'### {tot_freq_nome_1:,}'.replace(",", "."))
+            col11.markdown(f'### {int(dicionario_full[f'Frequência | {nome_1.capitalize()}'].sum()):,}'.replace(",", "."))
             st.divider()
 
-#Criação dos DF para facilitar a tabela(Poderia ser mais pythônico.).
-
-            df0=pd.DataFrame(resposta[0]['res'])
-            df0=df0.rename(columns={'periodo':'Período', 'frequencia': 'Frequência'})
-            df0['Período']=df0['Período'].str.replace('[',"" and ',').str.replace(',',' - ')
-            df0.set_index('Período', inplace=True)
-
-            df1=pd.DataFrame(resposta[1]['res'])
-            df1=df1.rename(columns={'periodo':'Período', 'frequencia': 'Frequência'})
-            df1['Período']=df1['Período'].str.replace('[',"" and ',').str.replace(',',' - ')
-            df1.set_index('Período', inplace=True)
 # Qual nome foi mais popular em cada período.
-            df_03 = pd.DataFrame({f'Frequência {nome_0}':df0['Frequência'],f'Frequência {nome_1}':df1['Frequência']})
+            df = pd.DataFrame(dicionario_full)
+            st.dataframe(df)
             
-#Criar a coluna 'Nome mais popular' usando o df_03.
-#Aplique no df_03, para cada linha da coluna 'Nome mais popular', conforme o caso:
-#nome_0 SE a linha 
-
-            df_03['Nome mais popular'] = df_03.apply(lambda row : nome_0.capitalize() if row[f'Frequência {nome_0}']> row [f'Frequência {nome_1}'] else nome_1.capitalize(), axis=1)
-            st.markdown("## Nome mais popular por período")
-            
-            df_03[f'Frequência {nome_0}'] = df_03[f'Frequência {nome_0}'].map(lambda x: f"{x:,}".replace(",", "."))
-            df_03[f'Frequência {nome_1}'] = df_03[f'Frequência {nome_1}'].map(lambda x: f"{x:,}".replace(",", "."))
-            st.dataframe(df_03)
+    if enviar and nome_0!="" or nome_1!="":
+        st.warning('Preencha os dois campos com nomes')
+    
+        
 except requests.exceptions.JSONDecodeError:
     pass
-
+except IndexError:
+    st.error('Um dos nomes não foi encontrado na base do IBGE.')
