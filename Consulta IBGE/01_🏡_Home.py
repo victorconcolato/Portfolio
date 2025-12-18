f1ac4807c3863755e46142fd2ed194e5057ffa2c
victorconcolato <victorconcolato@gmail.com>
2025-12-18 01:50:42 -0300

    Update 01_🏡_Home.py
    Atualização pagina Home

--- Consulta IBGE/01_🏡_Home.py
+++ Consulta IBGE/01_🏡_Home.py
@@ -1,16 +1,52 @@
 import streamlit as st
 
-st.markdown('# Bem vindo!')
-st.markdown('# Neste app, você poderá verifica o histórico de utilização de qualquer nome, usando a base do IBGE')
-st.markdown('# O período vai de 1930 a 2010, por década.')
-st.markdown('# Perguntas Frequentes:')
-st.markdown("""####
-            Quando consulto nomes compostos, por exemplo, Maria dos Anjos, Maria Luiza etc, a API não retorna nenhum resultado. Por quê ?      
-                → O Censo Demográfico 2010 não considerou nos questionários nomes compostos, apenas o primeiro nome e o último sobrenome. Por essa razão, esta API não retorna resultados ao consultar nomes compostos.
+st.markdown('### Bem vindo!')
+st.markdown('### Neste app, você poderá verifica o histórico de utilização de qualquer nome, usando a base do IBGE')
+st.markdown('### Período considerado: 1930 a 2010, por década.')
+st.markdown('### Considerações importantes:')
+
+st.markdown('##### → O Censo Demográfico 2010 não considerou nos questionários nomes compostos. Por esta razão, este programa não retorna resultados ao consultar nomes compostos.')
             
-            A API distingue nomes diferenciados somente pelo uso de sinais diacríticos (acento agudo, acento circunflexo, acento grave, cedilha, trema e til) como, por exemplo, Antônio e Antonio ?
+st.markdown('##### → A API não considera o uso de sinais diacríticos(acentos, cedilha, trema e til), de forma que Antônio e Antonio são contabilizados como Antonio.')
 
-                → A API não considera o uso de sinais diacríticos, de forma que Antônio e Antonio são contabilizados como Antonio. 
-            """)
 
-st.markdown('###### Produzido por (Victor Viana Concolato)[https://github.com/victorconcolato]')
+col01, col02, col03= st.columns(3)
+
+
+col01.markdown(
+    """
+<a href="https://github.com/victorconcolato">
+<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="25">
+</a>""",
+unsafe_allow_html=True
+)
+
+col02.markdown(
+    """
+    <a href="https://wa.me/5521983811828">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width="25">
+    </a>
+    """,
+    unsafe_allow_html=True
+)
+
+col03.markdown(
+    """
+<a href="https://www.instagram.com/victor.vianac/">
+<img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" width="25">
+</a>""",
+unsafe_allow_html=True
+)
+
+
+
+
+
+
+
+# col01.markdown(f'(st.image("https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg", width=25))[https://wa.me/5521983811828]')
+
+
+# col02.image("https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png", width=25)
+# col03.image("https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png", width=25)
+
