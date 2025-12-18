f6709e16b30b1beb9bad3d21a388c417c26e0bb5
victorconcolato <victorconcolato@gmail.com>
2025-12-18 01:14:25 -0300

    Update 01_🏡_Home.py

--- Consulta IBGE/01_🏡_Home.py
+++ Consulta IBGE/01_🏡_Home.py
@@ -1,11 +1,16 @@
 import streamlit as st
-import requests
-from pprint import pprint
 
-nome = 'joao'
-url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
+st.markdown('# Bem vindo!')
+st.markdown('# Neste app, você poderá verifica o histórico de utilização de qualquer nome, usando a base do IBGE')
+st.markdown('# O período vai de 1930 a 2010, por década.')
+st.markdown('# Perguntas Frequentes:')
+st.markdown("""####
+            Quando consulto nomes compostos, por exemplo, Maria dos Anjos, Maria Luiza etc, a API não retorna nenhum resultado. Por quê ?      
+                → O Censo Demográfico 2010 não considerou nos questionários nomes compostos, apenas o primeiro nome e o último sobrenome. Por essa razão, esta API não retorna resultados ao consultar nomes compostos.
+            
+            A API distingue nomes diferenciados somente pelo uso de sinais diacríticos (acento agudo, acento circunflexo, acento grave, cedilha, trema e til) como, por exemplo, Antônio e Antonio ?
 
-resposta = requests.get(url).json()
-pprint(resposta)
-pprint(resposta[0]['res'][1]['frequencia'])
+                → A API não considera o uso de sinais diacríticos, de forma que Antônio e Antonio são contabilizados como Antonio. 
+            """)
 
+st.markdown('###### Produzido por (Victor Viana Concolato)[https://github.com/victorconcolato]')
